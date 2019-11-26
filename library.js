66f8fe00507b7e1e452577de37b134023d814a34
LuisAlvarez99 <luis.alvarez0129@outlook.com>
2019-11-26 15:55:46 -0500

    changed name of js

--- library.js
+++ library.js
@@ -167,3 +167,32 @@ fetch('https://fizal.me/pokeapi/api/v2/id/429.json')
 })
 }
 pokemons()
+
+class Leader {
+  constructor(name){
+    this.name = name
+    this.team = []
+  }
+}
+
+
+class Pokemon {
+  constructor() {
+    this.name = ''
+  }
+}
+
+class Trainer {
+  constructor(name) {
+    this.name = name
+    this.team = []
+  }
+}
+
+
+let trainer = new Trainer()
+let dude = new Pokemon()
+let luisA = new Leader('Lewis A')
+let xaiver = new Leader('Xaiver')
+let eshwar = new Leader('Eshwar')
+let luisC = new Leader('Luis')
