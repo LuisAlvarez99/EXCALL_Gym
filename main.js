076c8d635414d2d77f6385e42e9601db23f3873e
LuisAlvarez99 <luis.alvarez0129@outlook.com>
2019-11-26 14:02:07 -0500

    created potential classes

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-// I will work here
+class Leader {
+  constructor(name){
+    this.name = name
+    this.team = []
+  }
+}
+
+
+class Pokemon {
+  constructor() {
+    this.name = ''
+  }
+}
+
+class Trainer {
+  constructor(name) {
+    this.name = name
+    this.team = []
+  }
+}
+
+
+let trainer = new Trainer()
+let dude = new Pokemon()
+let luisA = new Leader('Lewis A')
+let xaiver = new Leader('Xaiver')
+let eshwar = new Leader('Eshwar')
+let luisC = new Leader('Luis')
