54983f02ecb5881884d454cf81f42d956f63f295
LuisAlvarez99 <luis.alvarez0129@outlook.com>
2019-12-01 22:47:44 -0500

    modals work for each leader now

--- library.js
+++ library.js
@@ -234,18 +234,172 @@ let luisC = new Leader('Luis')
 
 
 
-let myModal = document.getElementById('myModal')
-let but = document.getElementById('myBtn')
-let span = document.getElementsByClassName('close')[0]
+let myModal1 = document.getElementById('myModal1')
+let myModal2 = document.getElementById('myModal2')
+let myModal3 = document.getElementById('myModal3')
+let myModal4 = document.getElementById('myModal4')
+let but1 = document.getElementById('myBtn1')
+let but2 = document.getElementById('myBtn2')
+let but3 = document.getElementById('myBtn3')
+let but4 = document.getElementById('myBtn4')
+let span1 = document.getElementsByClassName('close')[0]
+let span2 = document.getElementsByClassName('close')[1]
+let span3 = document.getElementsByClassName('close')[2]
+let span4 = document.getElementsByClassName('close')[3]
+let inside1 = document.getElementsByClassName('inner-modal')[0]
+let inside2 = document.getElementsByClassName('inner-modal')[1]
+let inside3 = document.getElementsByClassName('inner-modal')[2]
+let inside4 = document.getElementsByClassName('inner-modal')[3]
 
-but.onclick = function() {
-  myModal.style.display = 'block'
+
+function pokeStats1() {
+  let div = document.createElement('div')
+  inside1.appendChild(div)
+  div.innerHTML = `<p> ${xaiver.team[0].name}</p>
+                    <img src=${xaiver.team[0].sprites.front_default}>
+                    <p>HP: ${xaiver.team[0].stats[5].base_stat}</p>
+                    <p>ATK: ${xaiver.team[0].stats[4].base_stat}</p>
+                    <p>DEF: ${xaiver.team[0].stats[3].base_stat}</p>`
 }
-span.onclick = function() {
-  myModal.style.display = 'none'
+function pokeStats2() {
+  let div = document.createElement('div')
+  inside1.appendChild(div)
+  div.innerHTML = `<p> ${xaiver.team[1].name}</p>
+                    <img src=${xaiver.team[1].sprites.front_default}>
+                    <p>HP: ${xaiver.team[1].stats[5].base_stat}</p>
+                    <p>ATK: ${xaiver.team[1].stats[4].base_stat}</p>
+                    <p>DEF: ${xaiver.team[1].stats[3].base_stat}</p>`
 }
-window.onclick = function(event) {
-  if (event.target == myModal) {
-    modal.style.display = "none";
-  }
+function pokeStats3() {
+  let div = document.createElement('div')
+  inside1.appendChild(div)
+  div.innerHTML = `<p> ${xaiver.team[2].name}</p>
+                    <img src=${xaiver.team[2].sprites.front_default}>
+                    <p>HP: ${xaiver.team[2].stats[5].base_stat}</p>
+                    <p>ATK: ${xaiver.team[2].stats[4].base_stat}</p>
+                    <p>DEF: ${xaiver.team[2].stats[3].base_stat}</p>`
+}
+
+but1.onclick = function() {
+  myModal1.style.display = 'block'
+  pokeStats1()
+  pokeStats2()
+  pokeStats3()
+}
+span1.onclick = function() {
+  myModal1.style.display = 'none'
+}
+
+function pokeStats4() {
+  let div = document.createElement('div')
+  inside2.appendChild(div)
+  div.innerHTML = `<p> ${luisC.team[0].name}</p>
+                    <img src=${luisC.team[0].sprites.front_default}>
+                    <p>HP: ${luisC.team[0].stats[5].base_stat}</p>
+                    <p>ATK: ${luisC.team[0].stats[4].base_stat}</p>
+                    <p>DEF: ${luisC.team[0].stats[3].base_stat}</p>`
+}
+function pokeStats5() {
+  let div = document.createElement('div')
+  inside2.appendChild(div)
+  div.innerHTML = `<p> ${luisC.team[1].name}</p>
+                    <img src=${luisC.team[1].sprites.front_default}>
+                    <p>HP: ${luisC.team[1].stats[5].base_stat}</p>
+                    <p>ATK: ${luisC.team[1].stats[4].base_stat}</p>
+                    <p>DEF: ${luisC.team[1].stats[3].base_stat}</p>`
+}
+function pokeStats6() {
+  let div = document.createElement('div')
+  inside2.appendChild(div)
+  div.innerHTML = `<p> ${luisC.team[2].name}</p>
+                    <img src=${luisC.team[2].sprites.front_default}>
+                    <p>HP: ${luisC.team[2].stats[5].base_stat}</p>
+                    <p>ATK: ${luisC.team[2].stats[4].base_stat}</p>
+                    <p>DEF: ${luisC.team[2].stats[3].base_stat}</p>`
+}
+
+but2.onclick = function() {
+  myModal2.style.display = 'block'
+  pokeStats4()
+  pokeStats5()
+  pokeStats6()
+}
+span2.onclick = function() {
+  myModal2.style.display = 'none'
+}
+
+function pokeStats7() {
+  let div = document.createElement('div')
+  inside3.appendChild(div)
+  div.innerHTML = `<p> ${eshwar.team[0].name}</p>
+                    <img src=${eshwar.team[0].sprites.front_default}>
+                    <p>HP: ${eshwar.team[0].stats[5].base_stat}</p>
+                    <p>ATK: ${eshwar.team[0].stats[4].base_stat}</p>
+                    <p>DEF: ${eshwar.team[0].stats[3].base_stat}</p>`
+}
+function pokeStats8() {
+  let div = document.createElement('div')
+  inside3.appendChild(div)
+  div.innerHTML = `<p> ${eshwar.team[1].name}</p>
+                    <img src=${eshwar.team[1].sprites.front_default}>
+                    <p>HP: ${eshwar.team[1].stats[5].base_stat}</p>
+                    <p>ATK: ${eshwar.team[1].stats[4].base_stat}</p>
+                    <p>DEF: ${eshwar.team[1].stats[3].base_stat}</p>`
+}
+function pokeStats9() {
+  let div = document.createElement('div')
+  inside3.appendChild(div)
+  div.innerHTML = `<p> ${eshwar.team[2].name}</p>
+                    <img src=${eshwar.team[2].sprites.front_default}>
+                    <p>HP: ${eshwar.team[2].stats[5].base_stat}</p>
+                    <p>ATK: ${eshwar.team[2].stats[4].base_stat}</p>
+                    <p>DEF: ${eshwar.team[2].stats[3].base_stat}</p>`
+}
+
+but3.onclick = function() {
+  myModal3.style.display = 'block'
+  pokeStats7()
+  pokeStats8()
+  pokeStats9()
+}
+span3.onclick = function() {
+  myModal3.style.display = 'none'
+}
+
+function pokeStats10() {
+  let div = document.createElement('div')
+  inside4.appendChild(div)
+  div.innerHTML = `<p> ${luisA.team[0].name}</p>
+                    <img src=${luisA.team[0].sprites.front_default}>
+                    <p>HP: ${luisA.team[0].stats[5].base_stat}</p>
+                    <p>ATK: ${luisA.team[0].stats[4].base_stat}</p>
+                    <p>DEF: ${luisA.team[0].stats[3].base_stat}</p>`
+}
+function pokeStats11() {
+  let div = document.createElement('div')
+  inside4.appendChild(div)
+  div.innerHTML = `<p> ${luisA.team[1].name}</p>
+                    <img src=${luisA.team[1].sprites.front_default}>
+                    <p>HP: ${luisA.team[1].stats[5].base_stat}</p>
+                    <p>ATK: ${luisA.team[1].stats[4].base_stat}</p>
+                    <p>DEF: ${luisA.team[1].stats[3].base_stat}</p>`
+}
+function pokeStats12() {
+  let div = document.createElement('div')
+  inside4.appendChild(div)
+  div.innerHTML = `<p> ${xaiver.team[2].name}</p>
+                    <img src=${xaiver.team[2].sprites.front_default}>
+                    <p>HP: ${xaiver.team[2].stats[5].base_stat}</p>
+                    <p>ATK: ${xaiver.team[2].stats[4].base_stat}</p>
+                    <p>DEF: ${xaiver.team[2].stats[3].base_stat}</p>`
+}
+
+but4.onclick = function() {
+  myModal4.style.display = 'block'
+  pokeStats10()
+  pokeStats11()
+  pokeStats12()
+}
+span4.onclick = function() {
+  myModal4.style.display = 'none'
 }
